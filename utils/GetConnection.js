98c87b72df89a0edc2c2c37c3d40d5535102f38a
vasu-mandaviya <vasumandaviya111@gmail.com>
2025-01-06 22:41:08 +0530

    Edit Connection2

--- utils/GetConnection.js
+++ utils/GetConnection.js
@@ -5,12 +5,12 @@ const mongoURI = process.env.MONGO_URI;
 
 const GetConnection = async () => {
 
-     await mongoose.connect( mongoURI)
-          .then(() => {
-               console.log('Connected to MongoDB successfully!');
-          }).catch((error) => {
-               console.error('Error connecting to MongoDB:', error);
-          });
+     try {
+          await mongoose.connect(mongoURI)
+          console.log('Connected to MongoDB successfully!');
+     } catch (error) {
+          console.log('Error connecting to MongoDB:', error);
+     }
 
 }
 
