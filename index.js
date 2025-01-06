7cdf7903ea70ca7943328676657547cb8b38d094
vasu-mandaviya <vasumandaviya111@gmail.com>
2025-01-06 22:53:22 +0530

    Edit Connection4

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 let port = process.env.PORT || 3001;
 
 const express = require('express');
@@ -9,7 +10,9 @@ const { app, server } = require("./socket/index");
 // app.use(cors({
 //      origin: 'https://real-time-chat-frontend-chi.vercel.app'
 // }));
-app.use(cors());
+app.use(cors({
+     origin : process.env.FRONTEND_URL
+}));
 
 app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
